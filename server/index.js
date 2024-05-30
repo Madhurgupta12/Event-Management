0148c2856243a920cdaa06d9d35b94cf51f0427d
Ishsinghal <ishsinghal0707@gmail.com>
2024-05-30 17:34:57 +0530

    Made the Routes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const dotenv=require('dotenv');
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+const userRouter=require("./routes/user");
 const {connectDB}=require("./db/db")
+app.use("/user",userRouter);
 app.listen(process.env.PORT,(req,res)=>{
     console.log(`sever is running on port ${process.env.PORT}`);
 })
