a1858630646e13539b4dd78f3813039c1e345f99
Madhurgupta12 <om96002@gmail.com>
2024-05-30 19:37:10 +0530

    fix bug

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 const userRouter=require("./routes/user");
 const {connectDB}=require("./db/db")
-app.use("/user",userRouter);
+app.use("/api",userRouter);
 app.listen(process.env.PORT,(req,res)=>{
     console.log(`sever is running on port ${process.env.PORT}`);
 })
