6ce520010091b14d5703a69b6c33837b1c1e00c1
Madhurgupta12 <om96002@gmail.com>
2024-05-31 01:54:10 +0530

    backend setup for Task

--- server/routes/task.js
+++ server/routes/task.js
@@ -21,8 +21,35 @@ return res.status(200).json({success:true});
         else
         return res.status(404).json({success:false});
         
+})
+router.get("/show",middleware,async(req,res)=>{
+    const tasks=await Task.find({user:req.user});
+    if(tasks)
+        {
+            return res.status(200).json({success:true,tasks:tasks});
 
-    
+        }
+        else
+        return res.status(404).json({success:false});
+
+})
+router.post("/delete",middleware,async(req,res)=>{
+    const {title}=req.body;
+    if(!title)
+        {
+            return res.status(404).json({message:"Please enter a title"});
+        }
+    const tt=await Task.findOne({user:req.user,title:title});
+    const deletedTask = await Task.findOneAndDelete({ _id:tt, user:req.user });
+    if(deletedTask)
+        {
+            return res.status(200).json({success:true});
+        }
+        else
+        return res.status(404).json({success:false});
+
+
+
 
 })
 module.exports =router;
