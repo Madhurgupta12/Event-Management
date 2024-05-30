e9883adc9b7d5cf21a8df10047de22e427beaa59
Madhurgupta12 <om96002@gmail.com>
2024-05-30 19:32:47 +0530

    Make signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,21 @@
 import React from 'react'
-<<<<<<< Updated upstream
 import {Routes,Route} from "react-router-dom"
 import Home from "./Component/Home"
 import Login from "./Component/login"
-=======
+import Signup from "./Component/Signup"
 import TextEditor from './pages/TextEditor'
->>>>>>> Stashed changes
 const App = () => {
   
   return (
-<<<<<<< Updated upstream
      <>
     <Routes>
   <Route path="/" element={<Home></Home>}></Route>
   <Route path="/login" element={<Login></Login>}></Route>
+  <Route path="/signup" element={<Signup></Signup>}></Route>
  </Routes>
 
    
     </>
-=======
-    <div>
-      <TextEditor/>
-      </div>
->>>>>>> Stashed changes
   )
 }
 
