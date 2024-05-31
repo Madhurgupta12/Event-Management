42767b0481965bef5ef8dfe1f81c8a9c92f6765b
Madhurgupta12 <om96002@gmail.com>
2024-05-31 11:48:03 +0530

    bug

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import {Routes,Route} from "react-router-dom"
 import Home from "./Component/Home"
-import Login from "./Component/Login"
+import Login from "./Component/login"
 import Signup from "./Component/Signup"
 import TextEditor from './pages/TextEditor'
 import Kanban from "./Component/KanBan"
